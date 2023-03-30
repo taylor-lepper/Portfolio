50133cd52f0168244a33678df124a162af77a3ae
Taylor <taylor.lepper@gmail.com>
2023-03-29 23:31:42 -0800

    started about me page

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,21 @@
+import React from "react";
+
+// import DesertVideo from "../public/desertBackground";
+
 export default function About() {
     return (
       <>
     <div className="text-center">
         <h1 className="text-4xl pt-8">About Me</h1>
+        {/* <div>
+          <video loop autoPlay>
+            <source
+              repeat
+              src="../public/desertBackground"
+              type="video/mp4"
+            />
+          </video>
+        </div> */}
     </div>
       </>
     );
