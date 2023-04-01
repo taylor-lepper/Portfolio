41dd0caeb54f2e2df19f3c447afe919ed6703b9e
Taylor <taylor.lepper@gmail.com>
2023-04-01 12:24:18 -0700

    made navbar componant

--- componants/font.js
+++ componants/font.js
@@ -8,10 +8,13 @@ function Font() {
           <h1 className={styles.text}>Welcome Home</h1>
           <div className={styles.text2}>
         <h1 className="text-2xl pt-8 text-gray-200">
-            Please fasten your seatbelts for the journey ahead
+          This oppurtunity comes only once in a blue moon:
           </h1>
           <h1 className="text-xl pt-4 text-gray-200">
-           This oppurtunity only comes once in a blue moon
+          the professional and personal portfolio for Taylor Lepper.
+          </h1>
+          <h1 className="text-xl pt-8 text-gray-200">
+        Enjoy :)
           </h1>
         </div>
         </div>
