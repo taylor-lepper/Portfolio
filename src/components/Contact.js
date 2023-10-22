6fa22436d59efc5dbf4993c65b9ac87782db5cb6
Taylor <taylor.lepper@gmail.com>
2023-10-21 23:43:51 -0700

    fixed spacing, updated contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -13,10 +13,10 @@ export default function Contact() {
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
           <div className="md:ml-4">
             <header className="">
-              <h1 className="text-gray-50 font-semibold text-2xl">
+              <h1 className="text-gray-50 font-semibold text-2xl md:text-4xl">
                 Get in touch, let&apos;s chat.
               </h1>
-              <p className="font-light text-base text-gray-200 mt-2">
+              <p className="font-light text-base md:text-xl text-gray-200 mt-2">
                 Fill in the form to send me a message!
               </p>
             </header>
@@ -35,7 +35,7 @@ export default function Contact() {
                     d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"
                   />
                 </svg>
-                <p className="text-gray-50 font-light text-sm">
+                <p className="text-gray-50 font-light text-sm md:text-xl">
                   {userData.phone}
                 </p>
               </div>
@@ -50,7 +50,7 @@ export default function Contact() {
                 >
                   <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555zM0 4.697v7.104l5.803-3.558L0 4.697zM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757zm3.436-.586L16 11.801V4.697l-5.803 3.546z" />
                 </svg>
-                <p className="text-gray-50 font-light text-sm">
+                <p className="text-gray-50 font-light text-sm md:text-xl">
                   {userData.email}
                 </p>
               </div>
@@ -65,7 +65,7 @@ export default function Contact() {
                 >
                   <path d="M4.146.146A.5.5 0 0 1 4.5 0h7a.5.5 0 0 1 .5.5c0 .68-.342 1.174-.646 1.479-.126.125-.25.224-.354.298v4.431l.078.048c.203.127.476.314.751.555C12.36 7.775 13 8.527 13 9.5a.5.5 0 0 1-.5.5h-4v4.5c0 .276-.224 1.5-.5 1.5s-.5-1.224-.5-1.5V10h-4a.5.5 0 0 1-.5-.5c0-.973.64-1.725 1.17-2.189A5.921 5.921 0 0 1 5 6.708V2.277a2.77 2.77 0 0 1-.354-.298C4.342 1.674 4 1.179 4 .5a.5.5 0 0 1 .146-.354z" />
                 </svg>
-                <p className="text-gray-50 font-light text-sm">
+                <p className="text-gray-50 font-light text-sm md:text-xl">
                   {userData.address}
                 </p>
               </div>
@@ -142,7 +142,7 @@ export default function Contact() {
             </div>
           </div>
           <form className="form rounded-lg bg-white p-4 flex flex-col">
-            <label htmlFor="name" className="text-sm text-gray-600 mx-4">
+            <label htmlFor="name" className="text-sm md:text-lg text-gray-600 mx-4">
               {" "}
               Your Name
             </label>
@@ -151,7 +151,7 @@ export default function Contact() {
               className="font-light rounded-md border focus:outline-none py-2 mt-2 px-1 mx-4 focus:ring-2 focus:border-none ring-blue-500"
               name="name"
             />
-            <label htmlFor="email" className="text-sm text-gray-600 mx-4 mt-4">
+            <label htmlFor="email" className="text-sm md:text-lg text-gray-600 mx-4 mt-4">
               Email
             </label>
             <input
@@ -161,7 +161,7 @@ export default function Contact() {
             />
             <label
               htmlFor="message"
-              className="text-sm text-gray-600 mx-4 mt-4"
+              className="text-sm md:text-lg text-gray-600 mx-4 mt-4"
             >
               Message
             </label>
@@ -173,7 +173,7 @@ export default function Contact() {
             ></textarea>
             <button
               type="submit"
-              className="bg-blue-500 rounded-md w-1/2 mx-4 mt-8 py-2 text-gray-50 text-xs font-bold"
+              className="bg-blue-500 rounded-md w-1/2 mx-4 mt-8 py-2 text-gray-50 text-xs md:text-lg font-bold"
             >
               Send Message
             </button>
