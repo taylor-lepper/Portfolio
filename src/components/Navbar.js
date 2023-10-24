c2b55e3cc94b29f96c647ceed0789e9b4414e86c
Taylor <taylor.lepper@gmail.com>
2023-10-24 01:03:43 -0700

    fixed navbar small issue

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -18,7 +18,7 @@ export default function Navbar() {
     <div className="max-w-6xl md:max-w-full md:px-40 mx-auto px-4 py-10 md:py-20">
       <div className="flex md:flex-row justify-between items-center">
         <div className="flex flex-col">
-          <Link href="/">
+          <Link className="hover:text-gray-400" href="/">
             <h1 className="font-semibold text-xl md:text-3xl dark:text-gray-100">
               {userData.name}
             </h1>
